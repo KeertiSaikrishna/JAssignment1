70dd7c37aba0602ee69ca7edc4512e878964ce3d
Saikrishna <saikrishnakeerti@gmail.com>
2018-09-17 12:22:17 +0530

    Add files via upload

--- CheckDirectories.java
+++ CheckDirectories.java
@@ -4,34 +4,38 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckDirectories {
-    Scanner sc = new Scanner(System.in);
-    void Findfiles(String DirectoryName){
 
+    static void Findfiles(String DirectoryName) {
         String str = new String();
+        Scanner sc = new Scanner(System.in);
+
         str = sc.nextLine();
+        //accepting the pattern
         String temp = new String();
+        //creating a temporary String variable for getting the name of each file
+        Pattern p = Pattern.compile(".*" + str + ".*");
+        //creating a pattern to search
         File directory = new File(DirectoryName);
-        boolean b = true;
+        boolean matched = true;
         File[] fList = directory.listFiles();
-        for (File file : fList){
+        for (File file : fList) {
             temp = file.getName();
-            Pattern p = Pattern.compile(".*"+str+".*");
-            Matcher m =p.matcher(temp);
-            while (m.find()){
-                b = false;
+            Matcher m = p.matcher(temp);
+            while (m.find()) {
+                matched = false;
                 System.out.println(file.getAbsolutePath());
             }
         }
-        if(b)
-            System.out.println("file not found");
+        // iterating over the list of files in the specified directory and searching it with the given pattern
+        if (matched)
+            System.out.println("pattern not found in any of the file");
     }
 
     public static void main(String[] args) {
 
-        final String dir ="/home/user/IdeaProjects/incre-assignment7";
+        final String dir = "/home/zadmin/Downloads";
+        //path of the directory to be searched
 
-        CheckDirectories cd = new CheckDirectories();
-
-        cd.Findfiles(dir);
+        Findfiles(dir);
     }
 }
